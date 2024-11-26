945a6ee8ac57959ed50a1837122fc1d748096496
aron7 <aron@beeldr.nl>
2024-11-26 09:40:45 +0100

    ik mijn code wat getweaked

--- JS/project.js
+++ JS/project.js
@@ -1,13 +1,7 @@
 let projectInfo = [
   {
-      "name": "p5 Bounce",
-      "info": "We kregen op school de opdracht om met p5 te werken. Dit vond ik zo interessant dat ik deze animatie in mijn vrije tijd heb gemaakt om zo mijn kennis met p5 te vergroten",
-      "img": "bounce",
-      "link": "https://89031.stu.sd-lab.nl/2de_klas/beroeps/filmfestival/animatie/kaats.html?classId=6f628ba6-d956-4d1c-9cc8-78f472db4b06&assignmentId=08a501aa-75a9-42cd-817f-7a5fcb6e5913&submissionId=cfb71827-0a2f-1656-b168-a1b6defe4c97"
-  },
-  {
       "name": "undertaleFight",
-      "info": "Dit was mijn eerste echte grote project buiten school. Ik wilde mijn codeer kennis uitbreiden dus besloot ik een game te maken in HTML met CSS en javascript.",
+      "info": "Dit was een van mijn eerste afgemaakte projecten uit mijn vrije tijd. Dit is een na maak van een spel genaamd undertale. Let wel dit was toen ik net begon met coderen.",
       "img": "sans",
       "link": "https://stevejaws.github.io/undertalefight1/battle.html"
   }
